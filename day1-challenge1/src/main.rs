54f2a4fcb4490edd9b633cc44c1ab327b42cb15c
kim <taengoe@gmail.com>
2018-12-01 12:12:26 +0100

    day1 challenge1 done

---
+++ day1-challenge1/src/main.rs
@@ -0,0 +1,20 @@
+use std::fs;
+
+fn main() {
+    let content = fs::read_to_string("puzzleinput")
+        .expect("error reading file");
+    let mut frequency: i32 = 0;
+
+    for l in content.lines() {
+        match l.parse::<i32>() { // safe boys
+            Ok(n) => {
+                let beforechange = frequency; 
+                frequency += n;
+                println!("Current frequency {}, change of {}, resulting {}.", beforechange, l, frequency);
+            }
+            Err(e) => println!("{}", e)
+        }
+    }
+
+    println!("{}", frequency)
+}
