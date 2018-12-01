cf504a4686e3b48993897464cd471b16a463074f
kim <taengoe@gmail.com>
2018-12-01 17:07:54 +0100

    really slow and bad version

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,24 +1,43 @@
 use std::fs;
 
 fn main() {
-    ch1();
-}
-
-fn ch1() {
     let content = fs::read_to_string("puzzleinput")
         .expect("error reading file");
-    let mut frequency: i32 = 0;
 
-    for l in content.lines() {
-        match l.parse::<i32>() { // safe boys
-            Ok(n) => {
-                let beforechange = frequency; 
-                frequency += n;
-                println!("Current frequency {}, change of {}, resulting {}.", beforechange, l, frequency);
+    let mut freqvec = Vec::new();
+    let mut frequencyfin: i32 = 0;
+    let mut frequency: i32 = 0;
+    let mut firsttwice: i32 = 0;
+    let mut iteration: i32 = 1;
+    let mut br = true;
+
+    while br {
+        println!("starting iteration: {}", iteration);
+        for l in content.lines() {
+            match l.parse::<i32>() {
+                Ok(n) => {
+                    // let beforechange = frequency; 
+                    frequency += n;
+                    if iteration == 1 {
+                        frequencyfin += n;
+                    }
+                    // println!("Current frequency {}, change of {}, resulting {}.", beforechange, l, frequency)
+                }
+                Err(e) => println!("{}", e)
             }
-            Err(e) => println!("{}", e)
+
+            for value in freqvec.iter() {
+                if *value == frequency && br { 
+                    firsttwice = frequency; 
+                    br = false;
+                }
+            }
+
+            freqvec.push(frequency);
         }
+        iteration += 1;
     }
 
-    println!("{}", frequency)
+    println!("challenge 1 answer: {}", frequencyfin);
+    println!("challenge 2 answer: {}", firsttwice);
 }
