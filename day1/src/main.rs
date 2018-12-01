e298a3612feac7d0df3086dfe9762aa47b4606e0
kim <taengoe@gmail.com>
2018-12-01 21:49:25 +0100

    I am not happy with it but it will have to do

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,26 +2,29 @@ use std::fs;
 use std::collections::HashSet;
 
 fn main() {
-    let content = fs::read_to_string("puzzleinput2")
+    let content = fs::read_to_string("puzzleinput")
         .expect("error reading file");
 
     let mut hashfreq = HashSet::new();
     let mut frequency: i32 = 0;
-    let mut iteration: bool = true;
-    let mut notreachtwice: bool = true;
+    let mut ch1hasvalue: bool = false; // a bit ugly but works
+    let mut freqnotfound: bool = true;
 
     let mut ch1: i32 = 0;
+    let mut ch2: i32 = 0;
 
-    while notreachtwice {
+    while freqnotfound {
         for l in content.lines() {
-            frequency += l.parse::<i32>().unwrap();
+            frequency += l.parse::<i32>().unwrap(); // uneccssary parsing for every iteration
 
-            notreachtwice = hashfreq.insert(frequency);
-            if ! notreachtwice { break }
+            freqnotfound = hashfreq.insert(frequency);
+            if ! freqnotfound { 
+                ch2 = frequency; 
+                if ch1hasvalue { break }
+            }
         }
-        if iteration { ch1 = frequency; iteration = false }
+        if ! ch1hasvalue { ch1 = frequency; ch1hasvalue = true }
     }
-    let ch2 = frequency;
 
     println!("part 1 answer: {}", ch1);
     println!("part 2 answer: {}", ch2);
